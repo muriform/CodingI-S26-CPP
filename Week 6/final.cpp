998495086ad36ef3b0dc564846033239816d8ede
muriform <muriform07@outlook.com>
2026-05-05 14:07:05 -0500

    begin final project

---
+++ Week 6/final.cpp
@@ -0,0 +1,22 @@
+// Gavyn Garza Spring 26 Coding I
+// Final Assignment
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+// create a base class
+// private
+// set...
+// hunger
+// health
+// damage
+
+// default constrctor
+
