7704872a61548af7fa8f84a15fedb2b5edc7ff31
muriform <muriform07@outlook.com>
2026-05-08 14:01:28 -0500

    Coding I Final

--- Week 6/final.cpp
+++ Week 6/final.cpp
@@ -2,83 +2,58 @@
 // Final Assignment
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
-#include <ctime>
-#include <cstdlib>
+#include <ctime>     
+#include <cstdlib>   
 #include <algorithm>
-#include <fstream>
 
 using namespace std;
 
-// create a base class
-
-
-//     // default constrctor
-//     creature() {
-//         name = creatureNames[rand() % creatureNames.size()];
-//         hunger = rand() % 4 + 4;
-//         damage = rand() % 5 + 5;
-//         health = rand() % 4 + 4;
-//         startingHealth = health;
-//     }
-
-
-
-// };       // end of creature
-
-
-
-
-
-
-
-
-
-
+vector<string> names;
 
 class baseCreature {
 private:
+    //  base stats for the creatures.
     string name;
     int health;
-    int startingHealth = health;
-    int damage;
     int hunger;
+    int startingHealth = 0;
 
-    // constructor sets up the class with input variables, doesnt have a return type, must be named same as class
-    baseCreature(string givenName, int givenCharge, int givenBoredom) {
+public:
+    int damage;
+    // constructor
+    baseCreature(string givenName, int givenHealth, int givenHunger, int givenDamage) {
+        name = givenName;
+        health = givenHealth;
+        hunger = givenHunger;
+        damage = givenDamage;
+        
         cout << "A new creature has appeared...\n";
-        string name;
-        int health;
-        int startingHealth = 0;
-        int damage;
-        int hunger;
-        status();
-
     }
 
-    // default constructor
-    baseCreature() {
-        name = blank;
-        damage = rand() % 4 + 4;
-        health = rand() % 4 + 4;
-        startingHealth = health;
-        hunger = 1;
-        status();
+    void attack(baseCreature &opponent) {
+    
+        cout << name << " does " << damage << " base damage.\n";
+        cout << opponent.name << " has " << opponent.health << " health\n";
+
+            opponent.health -= damage;
+        
     }
 
     void status() {
-        cout << "Hello, my name is " << name;
-        cout << " and my health is " << health << ".\n";\
+        cout << "My health is " << health << ".\n";
+        cout << "I do " << damage << " base damage\n";
+        cout << "My hunger is " << hunger << ".\n";
         cout << "I am ";
 
-        if(hunger < 2) cout << "Full";
-        else if(hunger < 5) cout << "Peckish";
-        else if(hunger < 10) cout << "Famished";
-        else cout << "Starving";
-        cout << ".\n";
-
-        cout << "I do " << damage << " damage.\n";
+        if (hunger < 2)
+            cout << "Full...\n";
+        else if (hunger < 10)
+            cout << "Starving...\n";
+        else 
+            cout << "Cannot battle or train...\n";
     }
 
     // getters
@@ -87,142 +62,188 @@ private:
         return name;
     }
 
-    int getCharge() {
-        return charge;
+    int getHealth() {
+        return health;
     }
 
     int getHunger() {
         return hunger;
     }
+    int getDamage() {
+        return damage;
+    }
 
-    // setters
+    // setter
     void setName(string givenName) {
-        if(givenName.size() <= 5) {
+        if (givenName.size() <= 10) {
             name = givenName;
         }
-        else{
-            cout << "Error: " << givenName << " is too long.\n";
-        }
-    }
-
-    void setCharge(int givenCharge) {
-        // clamp charge to 0-100
-        if(givenCharge < 0) {
-            charge = 0;
-        
-        }
-        else if(givenCharge > 100) {
-            charge = 100;
-        }
         else {
-            charge = givenCharge;
+            cout << "error: " << givenName << "is too long.\n";
         }
     }
 
-    void setBoredom(int givenBoredom) {
-        if(givenBoredom > 20) boredom = 20;
-        boredom = givenBoredom;
-
+    void setHealth(int givenHealth) {
+        if (givenHealth < 0)
+            health = 0; 
+        else if (givenHealth > 100)
+            health = 100; 
+        else
+            health = givenHealth;
     }
 
+    void setHunger(int givenHunger) {
+        if (givenHunger > 20)
+            hunger = 20; 
+        hunger = givenHunger;
+    }
+
+    void setDamage(int givenDamage) {
+        if (givenDamage < 2)
+            damage = 0; 
+        else if (givenDamage > 50)
+            damage = 50; 
+        else
+            damage = givenDamage;
+    }
+};
+
+class bossCreature : public baseCreature {
+public:
+    int damageMultiplier = 2;
+
+    bossCreature()
+        : baseCreature("Meep", 55, 1, 4) {
+        damage *= damageMultiplier;
+    }
+
+    bossCreature(string givenName, int givenHealth, int givenHunger, int givenDamage)
+        : baseCreature(givenName, givenHealth, givenHunger, givenDamage) {
+        damage *= damageMultiplier;
+    }
+};
+
+vector<string> read(string path = "creatureNames.txt") {
+    string line;
+    vector<string> vec;
+    ifstream readFile(path);
+    if (readFile.is_open()) {
+        while (getline(readFile, line)) {
+            vec.push_back(line);
+        }
+        readFile.close();
+        return vec;
+    }
+    cout << "Sorry, I couldn't open that file.\n";
+
+    readFile.close();
+    return vec;
+}
+
+int main() {
+    string input;
+    srand(time(0)); 
+
+    cout << "What is your name?\n";
+    getline(cin, input);
+
+    cout << "You hear a rustle in the bushes...\n";
+
+    baseCreature baby("Baby", 10, 1, 10);
+    cout << "What would you like to name your new creature?\n";
+    cout << "The name must be less than 10 letters.\n";
+
+    cout << "Here's some examples: \n";
+    names = read();
+    for (int i = 0; i < names.size(); i++) {
+        cout << "\t" << names[i] << "\n";
+    }
+
+    getline(cin, input);
+    baby.setName(input);
+
+    cout << "You named your creature: " <<baby.getName() << endl;
+
+    do {
+        // if (baby.getHunger() > 5){
+        //     cout << "FEED ME" << endl;
+        //     cout <<"My hunger level is: " << baby.getHunger();
+        //     cout <<" I CANT FIGHT OR TRAIN IF MY HUNGER IS 10 OR MORE" << endl;
+        // }
+        cout << "What would you like to do?\n";
+        cout << "You can 'train', 'feed', 'listen', 'battle', or 'end'...\n";
+        getline(cin, input);
+
+        if (baby.getHunger() > 5) {
+            cout << "FEED ME" << endl;
+            cout <<"My hunger level is: " << baby.getHunger();
+            cout <<" I CANT FIGHT OR TRAIN IF MY HUNGER IS 10 OR MORE" << endl;
+        }
+
+        if (input == "train")  {
+            if (baby.getHunger() > 10){
+                cout << baby.getName() << " is too hungry to train..." << endl;
+                continue;
+            }
+            cout << baby.getName() << " grows ever stronger...\n";
+            baby.setHealth(baby.getHealth() + 3);
+            baby.setDamage(baby.getDamage() + 1);
+            baby.setHunger(baby.getHunger() + 1);
+        }
+
+        else if (input == "feed")  {
+            cout << "You have fed your creature...\n";
+            baby.setHunger(baby.getHunger() - 1);
+        }
+        else if (input == "listen")  {
+            baby.status();
+        }
+        else if (input == "battle") {
+            if (baby.getHunger() > 10){
+                cout << "I'm too hungry to battle " << endl;
+                continue;
+            }
+
+            bossCreature meep("Meep", 55, 1, 4);
+            cout << "This is a boss-type creature.\n";
+            
+
+            while (meep.getHealth() > 0 && baby.getHealth() > 0)   {
+                cout << "You can choose to 'attack', 'heal', or 'run'.\n";
+
+                getline(cin, input);
+                if(input == "attack") {
+                    baby.attack(meep); 
+                    cout << "The Meep now has " << meep.getHealth() << " health.\n";
+                    meep.attack(baby);
+                    cout << "Your creature has " << baby.getHealth() << " health left!\n";
+                }
+                else if(input == "heal") {
+                    cout << "Your creature is being healed...\n";
+                    baby.setHealth(baby.getHealth() + 5);
+                    cout << baby.getName() << " has " << baby.getHealth() << " health now.\n";
+                }
+                else if(input == "run") {
+                    cout << "You have ran away...\n";
+                    break;
+                }
+                else {
+                    cout << "The creature does not understand...\n";
+                }
+            
+            }
+        }
+
+        else if (input == "end")  {
+            return 0;
+        }
 
 
+        else {
+            cout << "The creature does not understand...\n";
+        }
 
+    } while (true);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// void read(vector<string>& vec, string path = "creatureNames.txt") {
-//     string line;
-//     ifstream readFile(path);
-//     if(readFile.is_open()) {
-//         cout << "The file is open.\n";
-//         while(getline(readFile, line)) {
-//             vec.push_back(line);        // add each line to the vector
-//         }
-//     }
-//     else {
-//         cout << "Couldn't open that file.\n";
-//     }
-
-//     // after done reading the file, close it
-
-//     readFile.close();
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int main() {
-//     srand(time(0)); 
-//     string input;
-//     vector<string> names;
-//     read(names);         // show the current names
-
-//     string path = "creatureNames.txt";
-
-//     creature creatureOne;
-//     creatureOne.name = "Meep";
-
-//     while(true) {
-//         cout << "What do you want to do?\n";
-        
-//         getline(cin, input);
-
-//         // name the critter
-//         if (input == "name") {
-//             cout << "What would you like to name your creature?\n";
-//             getline(cin, input);
-           
-//         }
-
-
-//         // feed the critter
-//         else if (input == "feed") {
-//             cout << "lalalalala\n"; 
-//         }
-
-
-//         // train critter (increase health or damage)
-//         else if (input == "train") {
-//             cout << "lalalalalala\n";
-//         }
-
-
-//         // listen to critter (display health, damage, hunger)
-//         else if (input == "listen") {
-//             cout << creatureOne.name << " is lalalalalala\n";
-//         }
-
-//     }
-
-
-// }       // end of main
+    return 0;
+}
